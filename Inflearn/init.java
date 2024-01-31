c0caade380fdb64659e385d5e2de08c5e21a29ee
minukbak <minukbak@gmail.com>
2024-01-31 21:46:36 +0900

    초기 코드 수정

--- Inflearn/init.java
+++ Inflearn/init.java
@@ -1,23 +1,24 @@
 /*
 Inflearn 자바 알고리즘 문제 실행기 초기 코드
-    - Main.java 파일로 실행
-(입력 = 백준 방식, 솔루션 = 프로그래머스 방식)
-
+  - Main.java 파일로 실행
+  - 입력 = 백준 방식, 솔루션 = 프로그래머스 방식
 [채점사이트](https://cote.inflearn.com/contest/10/problems)
 */
 import java.util.*;
 
 public class Main {
-  public int solution(String str, char t) {
-        int answer = 0;
-        
-        return answer;
-    }
+  public int solution(String str) {
+    int answer = 0;
+    
+    return answer;
+  }
 
-    public static void main(String[] args) {
-        Main T = new Main();
-        Scanner kb = new Scanner(System.in);
+  public static void main(String[] args) {
+    Main T = new Main();
+    Scanner kb = new Scanner(System.in);
 
-        System.out.print();
-    }
+    String str = kb.next();
+    
+    System.out.print(T.solution(str));
+  }
 }
