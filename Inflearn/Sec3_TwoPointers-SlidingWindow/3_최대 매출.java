e0b6f7b07cad87f561900ab4c7dd2000431b113a
minukbak <minukbak@gmail.com>
2024-03-07 00:17:10 +0900

    solved 3.3

--- Inflearn/Sec3_TwoPointers-SlidingWindow/3_최대 매출.java
+++ Inflearn/Sec3_TwoPointers-SlidingWindow/3_최대 매출.java
@@ -17,3 +17,35 @@
   - 예시 출력 1
     56
 */
+
+import java.util.*;
+
+class Main {
+  public int solution(int n, int k, int[] arr) {
+    int answer, sum = 0;
+
+    for (int i = 0; i < k; i++) sum += arr[i];
+
+    answer = sum;
+
+    for (int i = k; i < n; i++) {
+      sum += arr[i] - arr[i-k];
+      answer = Math.max(answer, sum);
+    }
+
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    Main T = new Main();
+    Scanner kb = new Scanner(System.in);
+    int n = kb.nextInt();
+    int K = kb.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = kb.nextInt();
+    }
+    
+    System.out.print(T.solution(n, K, arr));
+  }
+}
