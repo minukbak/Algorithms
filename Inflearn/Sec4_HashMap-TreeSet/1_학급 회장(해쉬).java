a995fc236f0592b1dd2e8dbcd820336a011ef24c
minukbak <minukbak@gmail.com>
2024-03-12 02:16:51 +0900

    solved 4.1

--- Inflearn/Sec4_HashMap-TreeSet/1_학급 회장(해쉬).java
+++ Inflearn/Sec4_HashMap-TreeSet/1_학급 회장(해쉬).java
@@ -16,3 +16,40 @@
   - 예시 출력 1
     C
 */
+
+import java.util.*;
+
+class Main {
+  public char solution(int n, String s) {
+    char answer = ' ';
+    HashMap<Character, Integer> map = new HashMap<>();
+
+    for (char x : s.toCharArray()) {
+      map.put(x, map.getOrDefault(x, 0) + 1);
+    }
+
+    // System.out.println(map.containsKey('F'));
+    // System.out.println(map.remove('A'));
+    // System.out.println(map.size());
+
+    int max = Integer.MIN_VALUE;
+    for (char key : map.keySet()) {
+      // System.out.println(key + " " + map.get(key));
+      if (map.get(key) > max) {
+         max = map.get(key);
+         answer = key;
+      }
+    }
+
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    Main T = new Main();
+    Scanner kb = new Scanner(System.in);
+    int n = kb.nextInt();
+    String str = kb.next();
+
+    System.out.println(T.solution(n, str));
+  }
+}
