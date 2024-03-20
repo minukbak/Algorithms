67565600256dae5346d77c6661f68aeb8f6a4d98
minukbak <minukbak@gmail.com>
2024-03-20 18:47:47 +0900

    solved 5.2

--- Inflearn/Sec5_Stack-Queue/2_괄호문자제거.java
+++ Inflearn/Sec5_Stack-Queue/2_괄호문자제거.java
@@ -11,3 +11,30 @@
   - 예시 출력 1
   EFLM
 */
+
+import java.util.*;
+
+class Main {
+  public String solution(String str) {
+    String answer = "";
+    Stack<Character> stack = new Stack<>();
+
+    for (char x : str.toCharArray()) {
+      if (x == ')') {
+        while (stack.pop() != '(');
+      }
+      else stack.push(x);
+    }
+    for (char x : stack) {
+      answer += x;
+    }
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    Main T = new Main();
+    Scanner kb = new Scanner(System.in);
+    String str = kb.next();
+    System.out.println(T.solution(str));
+  }
+}
