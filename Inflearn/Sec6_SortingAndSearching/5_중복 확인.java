b70fefe148bed4a1ae0a57a6c3642ebe2cd00ac8
minukbak <minukbak@gmail.com>
2024-04-04 00:26:48 +0900

    solved 6.5

--- Inflearn/Sec6_SortingAndSearching/5_중복 확인.java
+++ Inflearn/Sec6_SortingAndSearching/5_중복 확인.java
@@ -16,3 +16,26 @@
   - 예시 출력 1
     D
 */
+
+import java.util.*;
+
+class Main {
+  public String solution(int n, int[] arr) {
+    String answer = "U";
+
+    Arrays.sort(arr);
+    for (int i = 0; i < n - 1; i++) {
+      if (arr[i] == arr[i + 1]) return "D";
+    }
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    Main T = new Main();
+    Scanner kb = new Scanner(System.in);
+    int n = kb.nextInt();
+    int arr[] = new int[n];
+    for (int i = 0; i < n; i++) { arr[i] = kb.nextInt(); }
+    System.out.println(T.solution(n, arr));
+  }
+}
