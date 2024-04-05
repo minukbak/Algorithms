cf982888377dc6cd161aedd192a9fc69b49c425e
minukbak <minukbak@gmail.com>
2024-04-06 01:47:49 +0900

    Update init.java

--- Inflearn/init.java
+++ Inflearn/init.java
@@ -1,8 +1,9 @@
 /*
-Inflearn 자바 알고리즘 문제 실행기 초기 코드
-  - Main.java 파일로 실행
-  - 입력 = 백준 방식, 솔루션 = 프로그래머스 방식
-[채점사이트](https://cote.inflearn.com/contest/10/problems)
+  Inflearn 자바 알고리즘 문제 실행기 초기 코드
+    - Main.java 파일로 실행
+    - 입력 = 백준 방식, 솔루션 = 프로그래머스 방식
+  [채점사이트](https://cote.inflearn.com/contest/10/problems)
+    - 접속 불가 시 강의 내 섹션 0.채점사이트 연결 이용
 */
 import java.util.*;
 
