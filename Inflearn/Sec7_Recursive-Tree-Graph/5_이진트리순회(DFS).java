5199c99e30ece526bfcbca92dd27d72e631ad56d
minukbak <minukbak@gmail.com>
2024-05-04 00:38:49 +0900

    solved 7.5

--- Inflearn/Sec7_Recursive-Tree-Graph/5_이진트리순회(DFS).java
+++ Inflearn/Sec7_Recursive-Tree-Graph/5_이진트리순회(DFS).java
@@ -13,3 +13,43 @@
     중위순회 출력: 4 2 5 1 6 3 7
     후위순회 출력: 4 5 2 6 7 3 1
 */
+
+import java.util.*;
+
+class Node {
+  int data;
+  Node lt, rt;
+  public Node(int val) {
+    data = val;
+    lt = rt = null;
+  }
+}
+
+public class Main {
+  Node root;
+  public void DFS(Node root) {
+    if (root == null) return;
+    else {
+      // 전위순회 출력
+      System.out.print(root.data + " ");
+      DFS(root.lt);
+      // 중위순회 출력
+      // System.out.print(root.data + " ");
+      DFS(root.rt);
+      // 후위순회 출력
+      // System.out.print(root.data + " ");
+    }
+  }
+
+  public static void main(String[] args) {
+    Main tree = new Main();
+    tree.root = new Node(1);
+    tree.root.lt = new Node(2);
+    tree.root.rt = new Node(3);
+    tree.root.lt.lt = new Node(4);
+    tree.root.lt.rt = new Node(5);
+    tree.root.rt.lt = new Node(6);
+    tree.root.rt.rt = new Node(7);
+    tree.DFS(tree.root);
+  }
+}
