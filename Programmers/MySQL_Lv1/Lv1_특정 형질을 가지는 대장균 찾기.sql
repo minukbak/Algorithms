ac04b041bdab6a113950e2894dc484de2f2c1eb7
minukbak <minukbak@gmail.com>
2024-05-04 00:15:46 +0900

    solved on 240504

---
+++ Programmers/MySQL_Lv1/Lv1_특정 형질을 가지는 대장균 찾기.sql
@@ -0,0 +1,6 @@
+/* https://school.programmers.co.kr/learn/courses/30/lessons/301646 */
+
+-- 코드를 작성해주세요
+SELECT COUNT(*) AS COUNT
+FROM ECOLI_DATA
+WHERE (GENOTYPE & 2 = 0) AND ((GENOTYPE & 1 = 1) OR (GENOTYPE & 4 = 4))
