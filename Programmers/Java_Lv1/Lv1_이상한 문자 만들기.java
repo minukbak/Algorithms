3d01a057fe94c88f0b9672735944460b81df1c0a
minukbak <minukbak@gmail.com>
2024-05-20 15:34:28 +0900

    solved on 240520

---
+++ Programmers/Java_Lv1/Lv1_이상한 문자 만들기.java
@@ -0,0 +1,23 @@
+/* https://school.programmers.co.kr/learn/courses/30/lessons/12930 */
+
+class Solution {
+  public String solution(String s) {
+    String answer = "";
+    String[] str = s.split("");
+    
+    int idx = 0;
+    for (int i = 0; i < str.length; i++) {
+      if (str[i].equals(" ")) {
+        idx = 0;
+      } else if (idx % 2 == 0) {
+        str[i] = str[i].toUpperCase();
+        idx++;
+      } else { // (idx % 2 == 1)
+        str[i] = str[i].toLowerCase();
+        idx++;
+      }
+      answer += str[i];
+    }
+    return answer;
+  }
+}
