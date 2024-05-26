a1905e90016a7b1392259d4bf632ded688f1f45c
minukbak <minukbak@gmail.com>
2024-05-26 22:16:15 +0900

    solved on 240526

---
+++ Programmers/Java_Lv1/Lv1_짝수와 홀수.java
@@ -0,0 +1,11 @@
+/* https://school.programmers.co.kr/learn/courses/30/lessons/12937 */
+
+class Solution {
+  public String solution(int num) {
+    if (num % 2 == 0) {
+      return ("Even");
+    } else {
+      return ("Odd");
+    }
+  }
+}
