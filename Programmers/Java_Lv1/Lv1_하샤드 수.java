2630784f17cbdd747b5a98bab7a29d968fc45731
minukbak <minukbak@gmail.com>
2024-06-01 03:13:09 +0900

    solved on 240601

---
+++ Programmers/Java_Lv1/Lv1_하샤드 수.java
@@ -0,0 +1,14 @@
+/* https://school.programmers.co.kr/learn/courses/30/lessons/12947 */
+
+class Solution {
+  public boolean solution(int x) {
+    int num = x;
+    int sum = 0;
+
+    while (num > 0) {
+      sum += num % 10;
+      num /= 10;
+    }
+    return x % sum == 0 ? true : false;
+  }
+}
