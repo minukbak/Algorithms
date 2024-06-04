07803d329f24302164edea35d7e90ada83a6f5d3
minukbak <minukbak@gmail.com>
2024-06-04 23:42:44 +0900

    solved 7.12

---
+++ Inflearn/Sec7_Recursive-Tree-Graph/12_경로탐색(인접리스트, ArrayList).java
@@ -0,0 +1,76 @@
+/*
+12. 경로 탐색(인접 리스트)
+  - 설명
+    방향그래프가 주어지면 1번 정점에서 N번 정점으로 가는 모든 경로의 가지 수를 출력하는 프로그램을 작성하세요.
+    아래 그래프에서
+      1  ↔  2  →  5
+      ↓ ↘ ↙ ↑ ↗
+      3  →  4
+    1번 정점으로 가는 가지 수는
+      1 2 3 4 5
+      1 2 5
+      1 3 4 2 5
+      1 3 4 5
+      1 4 2 5
+      1 4 5  
+    총 6가지 입니다.
+  - 입력 설명
+    첫 째 줄에는 정점의 수 N(1<=N<=20)과 간선의 수 M이 주어진다. 그 다음부터 M 줄에 걸쳐 연결 정보가 주어진다. 
+  - 출력 설명
+    총 가지 수를 출력한다.
+  - 입력
+      5 9 
+      1 2 
+      1 3 
+      1 4 
+      2 1 
+      2 3 
+      2 5 
+      3 4 
+      4 2 
+      4 5 
+  - 출력
+      6 
+*/
+
+import java.util.*;
+
+public class Main {
+  static int n, m, answer = 0;
+  static ArrayList<ArrayList<Integer>> graph;
+  static int[] ch;
+  
+  public void DFS(int v) {
+    if (v == n) answer++;
+    else {
+      for (int nv : graph.get(v)) {
+        if (ch[nv] == 0) {
+          ch[nv] = 1;
+          DFS(nv);
+          ch[nv] = 0;
+        }
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    Main T = new Main();
+    Scanner kb = new Scanner(System.in);
+    n = kb.nextInt();
+    m = kb.nextInt();
+    graph = new ArrayList<ArrayList<Integer>>();
+    for (int i = 0; i <= n; i++) {
+      graph.add(new ArrayList<Integer>());
+    }
+    ch = new int[n + 1];
+    for (int i = 0; i < m; i++) {
+      int a = kb.nextInt();
+      int b = kb.nextInt();
+      graph.get(a).add(b);
+    }
+    ch[1] = 1;
+    T.DFS(1);
+
+    System.out.println(answer);
+  }
+}
