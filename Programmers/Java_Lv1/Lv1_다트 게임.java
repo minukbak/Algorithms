9240b712dad8ef7f8607d9e4fa8eaab0a6927556
minukbak <minukbak@gmail.com>
2024-06-06 23:33:27 +0900

    solved on 240606

---
+++ Programmers/Java_Lv1/Lv1_다트 게임.java
@@ -0,0 +1,41 @@
+/* https://school.programmers.co.kr/learn/courses/30/lessons/17682 */
+
+class Solution {
+  public int solution(String dartResult) {
+    int[] dart = new int[3];
+
+    int n = 0, idx = 0;
+    String numStr = "";
+    
+    for (int i = 0; i < dartResult.length(); i++) {
+      char c = dartResult.charAt(i);
+      
+      // 숫자
+      if (c >= '0' && c <= '9') {       
+        numStr += String.valueOf(c);
+      }
+      // 보너스
+      else if (c == 'S' || c == 'D' || c == 'T') {
+        n = Integer.parseInt(numStr);
+        if (c == 'S') {
+          dart[idx++] = (int) Math.pow(n,1);
+        } else if (c == 'D') {
+          dart[idx++] = (int) Math.pow(n,2);
+        } else {
+          dart[idx++] = (int) Math.pow(n,3);
+        } 
+        numStr = "";
+      }
+      // 옵션
+      else {
+        if (c == '*') {
+          dart[idx - 1] *= 2;
+          if (idx - 2 >= 0) dart[idx - 2] *= 2; 
+        } else { // c == '#'
+          dart[idx - 1] *= -1;
+        }
+      }
+    }
+    return dart[0] + dart[1] + dart[2];
+  }
+}
