5dceb6f525e9931683d5a152536da326edfa1253
minukbak <minukbak@gmail.com>
2024-06-20 23:43:48 +0900

    solved 8.5

--- Inflearn/Sec8_DFS-BFS/5_동전 교환.java
+++ Inflearn/Sec8_DFS-BFS/5_동전 교환.java
@@ -17,3 +17,33 @@
   - 힌트
     출력 설명 : 5 5 5 동전 3개로 거슬러 줄 수 있다.
 */
+
+import java.util.*;
+
+public class Main {
+  static int n, m, answer = Integer.MAX_VALUE;
+  
+  public void DFS(int L, int sum, Integer[] arr) {
+    if (sum > m) return;
+    if (L >= answer) return;
+    if (sum == m) {
+      answer = Math.min(answer, L);
+    } else {
+      for (int i = 0; i < n; i++) {
+        DFS(L + 1, sum + arr[i], arr);
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    Main T = new Main();
+    Scanner kb = new Scanner(System.in);
+    n = kb.nextInt();
+    Integer[] arr = new Integer[n];
+    for (int i = 0; i < n; i++) arr[i] = kb.nextInt();
+    Arrays.sort(arr, Collections.reverseOrder());
+    m = kb.nextInt();
+    T.DFS(0, 0, arr);
+    System.out.println(answer);
+  }
+}
