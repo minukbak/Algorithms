ec59f70cc33db6dcf4ebcde58b15ff49e909f950
minukbak <minukbak@gmail.com>
2024-06-22 21:09:52 +0900

    solved 8.8

--- Inflearn/Sec8_DFS-BFS/8_수열 추측하기.java
+++ Inflearn/Sec8_DFS-BFS/8_수열 추측하기.java
@@ -4,7 +4,7 @@
     가장 윗줄에 1부터 N까지의 숫자가 한 개씩 적혀 있다. 그리고 둘째 줄부터 차례대로 파스칼의 삼각형처럼 위의 두개를 더한 값이 저장되게 된다.
     예를 들어 N이 4 이고 가장 윗 줄에 3 1 2 4 가 있다고 했을 때, 다음과 같은 삼각형이 그려진다.
     3 1 2 4
-     3 4 6
+     4 3 6
       7 9
        16
     N과 가장 밑에 있는 숫자가 주어져 있을 때 가장 윗줄에 있는 숫자를 구하는 프로그램을 작성하시오.
@@ -20,3 +20,51 @@
   - 예시 출력 1
     3 1 2 4
 */
+
+import java.util.*;
+
+public class Main {
+  static int n, f;
+  static int[] b, pm, chk;
+  boolean flag = false;
+  int[][] dy = new int[35][35];
+
+  public int combi(int n, int r) {
+    if (dy[n][r] > 0) return dy[n][r];
+    if (n == r || r == 0) return 1;
+    else return dy[n][r] = combi(n - 1, r - 1) + combi(n - 1, r);
+  }
+
+  public void DFS(int L, int sum) {
+    if (flag) return;
+    if (L == n) {
+      if (sum == f) {
+        for (int x : pm) System.out.print(x + " ");
+        flag = true;
+      }
+    } else {
+      for (int i = 1; i <= n; i++) {
+        if (chk[i] == 0) {
+          chk[i] = 1;
+          pm[L] = i;
+          DFS(L + 1, sum + (pm[L] * b[L]));
+          chk[i] = 0;
+        }
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    Main T = new Main();
+    Scanner kb = new Scanner(System.in);
+    n = kb.nextInt();
+    f = kb.nextInt();
+    b = new int[n];
+    pm = new int[n];
+    chk = new int [n + 1];
+    for (int i = 0; i < n; i++) {
+      b[i] = T.combi(n - 1, i);
+    }
+    T.DFS(0, 0);
+  }
+}
