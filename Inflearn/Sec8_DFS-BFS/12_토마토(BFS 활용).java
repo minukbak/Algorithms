9a52015248699c302911618fa6842749755ec8ad
minukbak <minukbak@gmail.com>
2024-07-07 04:18:01 +0900

    solved 8.12

--- Inflearn/Sec8_DFS-BFS/12_토마토(BFS 활용).java
+++ Inflearn/Sec8_DFS-BFS/12_토마토(BFS 활용).java
@@ -29,3 +29,71 @@
   - 예시 출력 1
     4
 */
+
+import java.util.*;
+
+class Point {
+  public int x, y;
+  Point(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+public class Main {
+  static int[] dx = {-1, 0, 1, 0};
+  static int[] dy = {0, 1, 0, -1};
+  static int[][] board, day;
+  static int n, m;
+  static Queue<Point> Q = new LinkedList<>();
+
+  public void BFS() { 
+    while (!Q.isEmpty()) {
+      Point tmp = Q.poll();
+      for (int i = 0; i < 4; i++) {
+        int nx = tmp.x + dx[i];
+        int ny = tmp.y + dy[i];
+        if (nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == 0) {
+          board[nx][ny] = 1;
+          Q.offer(new Point(nx, ny));
+          day[nx][ny] = day[tmp.x][tmp.y] + 1;
+        }
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    Main T = new Main();
+    Scanner kb = new Scanner(System.in);
+    m = kb.nextInt();
+    n = kb.nextInt();
+    board = new int[n][m];
+    day = new int[n][m];
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        board[i][j] = kb.nextInt();
+        if (board[i][j] == 1) Q.offer(new Point(i, j));
+      }
+    }
+    T.BFS();
+
+    boolean flag = true;
+    int answer = Integer.MIN_VALUE;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if (board[i][j] == 0) flag = false;
+      }
+    }
+    
+    if (flag) {
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+          answer = Math.max(answer, day[i][j]);
+        }
+      }
+      System.out.println(answer);
+    }
+    else System.out.println(-1);
+  }
+}
