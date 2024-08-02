ccb5ebbd37d08d6f57342b9bca714890707dbfc7
minukbak <minukbak@gmail.com>
2024-08-03 00:04:09 +0900

    solved on 240803

--- Programmers/Java_Lv1/Lv1_문자열 나누기.java
+++ Programmers/Java_Lv1/Lv1_문자열 나누기.java
@@ -1,1 +1,22 @@
 /* https://school.programmers.co.kr/learn/courses/30/lessons/140108 */
+
+class Solution {
+  public int solution(String s) {
+    int answer = 1;
+    int count = 1;
+    char x = s.charAt(0);
+    
+    for (int i = 1; i < s.length(); i++) {
+      if (count == 0) {
+        answer++;
+        x = s.charAt(i);
+      }
+      if (x == s.charAt(i)) {
+        count++;
+      } else {
+        count--;
+      }
+    }
+    return answer;
+  }
+}
