02cad3fd30a7292948994fe4fb15a412e4c5a1a1
minukbak <minukbak@gmail.com>
2024-09-18 18:15:34 +0900

    solved on 240918

---
+++ Programmers/MySQL_Lv2/Lv2_이름에 el이 들어가는 동물 찾기.sql
@@ -0,0 +1,8 @@
+/* https://school.programmers.co.kr/learn/courses/30/lessons/59047 */
+
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE LOWER(NAME) LIKE '%el%'
+    AND ANIMAL_TYPE = 'Dog'
+ORDER BY NAME
+;
