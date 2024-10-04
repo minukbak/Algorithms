2a5bb94f48348248c927afedcc3c48e01f068956
minukbak <minukbak@gmail.com>
2024-10-04 22:41:10 +0900

    solved on 241004

---
+++ Programmers/MySQL_Lv2/Lv2_진료과별 총 예약 횟수 출력하기.sql
@@ -0,0 +1,9 @@
+/* https://school.programmers.co.kr/learn/courses/30/lessons/132202 */
+
+SELECT MCDP_CD AS '진료과코드', COUNT(APNT_NO) AS '5월예약건수'
+FROM APPOINTMENT
+WHERE YEAR(APNT_YMD) = '2022' AND MONTH(APNT_YMD) = '05'
+     -- AND APNT_CNCL_YN = 'N' AND APNT_CNCL_YMD IS NULL
+GROUP BY MCDP_CD
+ORDER BY COUNT(APNT_NO), MCDP_CD
+;
