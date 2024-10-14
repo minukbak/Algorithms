362bdc7aa024c5a6059bef7768366d5a0df0382b
minukbak <minukbak@gmail.com>
2024-10-15 02:40:34 +0900

    solved on 241015

---
+++ Programmers/MySQL_Lv2/LV2_ROOT 아이템 구하기.sql
@@ -0,0 +1,8 @@
+/* https://school.programmers.co.kr/learn/courses/30/lessons/273710 */
+
+SELECT A.ITEM_ID, A.ITEM_NAME
+FROM ITEM_INFO A, ITEM_TREE B
+WHERE A.ITEM_ID = B.ITEM_ID
+    AND B.PARENT_ITEM_ID IS NULL
+ORDER BY A.ITEM_ID
+;
