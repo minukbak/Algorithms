830a5e8592adced33625270b73ca0d6e19c60612
minukbak <minukbak@gmail.com>
2024-10-22 13:36:12 +0900

    solved on 241022

---
+++ Programmers/MySQL_Lv2/Lv2_물고기 종류 별 잡은 수 구하기.sql
@@ -0,0 +1,9 @@
+/* https://school.programmers.co.kr/learn/courses/30/lessons/293257 */
+
+SELECT COUNT(1) AS FISH_COUNT, NI.FISH_NAME
+FROM FISH_INFO I
+    JOIN FISH_NAME_INFO NI
+    ON I.FISH_TYPE = NI.FISH_TYPE
+GROUP BY NI.FISH_NAME
+ORDER BY COUNT(1) DESC
+;
