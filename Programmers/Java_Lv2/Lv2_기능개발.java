b56c42a9d6e0f5ae012dd8cf883d8b15487ff6f7
minukbak <minukbak@gmail.com>
2026-05-29 22:36:29 +0900

    solved on 260529

---
+++ Programmers/Java_Lv2/Lv2_기능개발.java
@@ -0,0 +1,8 @@
+/* https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java */
+
+class Solution {
+  public int[] solution(int[] progresses, int[] speeds) {
+      int[] answer = {};
+      return answer;
+  }
+}
